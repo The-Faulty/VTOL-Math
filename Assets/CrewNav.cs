a90c5d1a3edc522b57e7946bc01a53b24543e6ce
Andrew Levine <andydandy4001@gmail.com>
2023-03-16 09:38:57 -0400

    Slight CrewNav Change
    lemme unstash the changes :angry:

--- Assets/CrewNav.cs
+++ Assets/CrewNav.cs
@@ -22,11 +22,17 @@ public class CrewNav : MonoBehaviour
   private IEnumerator MoveToAsync(Vector3 pos)
   {
     //CharacterTransform.rotation = Vector3.
+    Vector3 lookPos;
+    Quaternion rotation;
     Vector3 startPos = CharacterTransform.position;
     float distance = Vector3.Distance(startPos, pos);
     remainingDistance = distance;
     while (remainingDistance > 0)
     {
+      lookPos = CharacterTransform.transform.position - pos;
+      lookPos.y = 0;
+      rotation = Quaternion.LookRotation(lookPos);
+      CharacterTransform.transform.rotation = Quaternion.Slerp(CharacterTransform.transform.rotation, rotation, Time.deltaTime * 2);
       CharacterTransform.transform.position = Vector3.Lerp(startPos, pos, 1 - (remainingDistance / distance));
       remainingDistance -= MoveSpeed * Time.deltaTime;
       yield return null;
